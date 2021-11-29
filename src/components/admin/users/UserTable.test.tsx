12bdd44b24b9572b5f6428d54fff93ac2984b5e8
ghost12137 <1292305370@qq.com>
2021-11-29 18:02:11 +0800

    修复ant无法单侧的问题

--- src/components/admin/users/UserTable.test.tsx
+++ src/components/admin/users/UserTable.test.tsx
@@ -3,20 +3,22 @@ import { render, screen, fireEvent, act } from '@testing-library/react';
 
 import UserTable from './UserTable';
 
-// beforeAll(() => {
-//   Object.defineProperty(window, "matchMedia", {
-//     value: jest.fn(() => { return { matches: true } })
-//   });
-// });
-
-// jest.mock('../../../services/admin/users.ts');
+window.matchMedia = (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(), // Deprecated
+  removeListener: jest.fn(), // Deprecated
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+});
 
 describe('UserTable单元测试', async () => {
   test('UserTable显示数据', async () => {
     await render(<UserTable />);
-    // screen.debug();
 
-    // const userName = await screen.findByText('李娟');
-    // expect(userName).toBeInTheDocument();
+    const userName = await screen.findByText('李娟');
+    expect(userName).toBeInTheDocument();
   });
 });
